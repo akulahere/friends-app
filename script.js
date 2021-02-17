fd27745df17d874327be0b5fe070174def25057d
dmitry <akulinind@gmail.com>
2021-02-17 18:12:46 +0300

    implement base rendering

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const getUsers = async () => {
     if (response.ok) {
       const responseJson = await response.json();
       users = responseJson.results;
+      console.log(users);
     } else {
       throw new Error(response.status);
     }
@@ -18,4 +19,36 @@ const getUsers = async () => {
   return users;
 }
 
-console.log(getUsers());
+const renderUsers = () => {
+  const usersList = document.querySelector('.users');
+  const fragment = document.createDocumentFragment();
+  if (usersList.innerHTML) usersList.innerHTML = '';
+  users.forEach(({ name, picture, dob, location, email }) => {
+    console.log(name)
+    const card = document.createElement('li');
+    card.classList.add('user-card')
+    card.innerHTML = `
+      <div class="photo-container">
+        <img class="photo" src="${picture.large}" alt="User photo">
+      </div>
+      <div class="user-info">
+        <p class="name">${name.first} ${name.last}</p>
+         <p class="age">${dob.age} years old</p>
+         <p class="location" title="${location.city}">${location.city}</p>
+        <a class="email" href="mailto:${email}">${email}</a>
+      </div>
+    `;
+    fragment.append(card)
+  });
+  usersList.append(fragment)
+};
+
+
+
+const initialize = async () => {
+  await getUsers();
+  renderUsers();
+}
+
+initialize()
+
