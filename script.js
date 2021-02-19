f66c6b3a731e820794951294324af0bcf403cc71
dmitry <akulinind@gmail.com>
2021-02-19 18:42:34 +0300

    implement base css + name/age sort

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let users;
+const nameSortButton = document.getElementById('name-sort');
+const ageSortButton = document.getElementById('age-sort');
+
 
 const getUsers = async () => {
   try {
@@ -6,7 +9,6 @@ const getUsers = async () => {
     if (response.ok) {
       const responseJson = await response.json();
       users = responseJson.results;
-      console.log(users);
     } else {
       throw new Error(response.status);
     }
@@ -14,7 +16,7 @@ const getUsers = async () => {
   catch (error) {
     const errorNotification = document.createElement('p');
     errorNotification.innerHTML = `Network error! Status code: ${error}`;
-    document.querySelector('main').prepend(errorNotification)
+    document.querySelector('main').prepend(errorNotification);
   }
   return users;
 }
@@ -24,9 +26,8 @@ const renderUsers = () => {
   const fragment = document.createDocumentFragment();
   if (usersList.innerHTML) usersList.innerHTML = '';
   users.forEach(({ name, picture, dob, location, email }) => {
-    console.log(name)
     const card = document.createElement('li');
-    card.classList.add('user-card')
+    card.classList.add('person-card')
     card.innerHTML = `
       <div class="photo-container">
         <img class="photo" src="${picture.large}" alt="User photo">
@@ -44,11 +45,45 @@ const renderUsers = () => {
 };
 
 
+const compareByName = (a, b) => a.name.first.toLowerCase() <= b.name.first.toLowerCase() ? -1 : 1;
+const compareByAge = (a, b) => a.dob.age - b.dob.age;
+
+
+const sortByName = () => {
+  if (nameSortButton.dataset.sortType !== 'A-Z') {
+    nameSortButton.dataset.sortType = 'A-Z';
+    users.sort((a, b) => compareByName(a, b));
+  } else {
+    nameSortButton.dataset.sortType = 'Z-A';
+    users.sort((a, b) => compareByName(b, a));
+  }
+};
+
+const sortByAge = () => {
+  if (ageSortButton.dataset.sortType !== '1-99') {
+    ageSortButton.dataset.sortType = '1-99';
+    users.sort((a, b) => compareByAge(a, b));
+  } else {
+    ageSortButton.dataset.sortType = '99-1';
+    users.sort((a, b) => compareByAge(b, a));
+  }
+};
+
+
 
 const initialize = async () => {
   await getUsers();
   renderUsers();
-}
+};
 
-initialize()
+initialize();
 
+nameSortButton.addEventListener('click', () => {
+  sortByName();
+  renderUsers();
+});
+
+ageSortButton.addEventListener('click', () => {
+  sortByAge();
+  renderUsers();
+});
